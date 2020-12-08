5d07eed320967860e5b76ad0433c33320b55433f
xuebingbing123 <2711227944@qq.com>
2020-12-08 14:57:19 +0800

    提交

---
+++ chapter6/src/main/java/com/test/demo/TestMethods.java
@@ -0,0 +1,22 @@
+package com.test.demo;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestMethods {
+
+    @Test
+    public void test1(){
+        Assert.assertEquals(1,2);
+    }
+    @Test
+    public void test2(){
+        Assert.assertEquals(1,1);
+    }
+    @Test
+    public void logDemo(){
+        Reporter.log("这是我自己写的日志");
+        throw new RuntimeException("这是我自己运行时的异常");
+    }
+}
